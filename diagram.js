9da21a3be6cd4d188b38826bcb14ccefbf4c1ecf
oscarerc <oscareduardorodriguezcastro3@gmail.com>
2022-08-13 11:12:42 -0500

    agregando diagrama

--- diagram.js
+++ diagram.js
@@ -1,4 +1,55 @@
-export function buildDiagram(){
-    Element
-    
+    // setup 
+    const data = ChartVenn.extractSets([
+        {
+            label:'A',
+            values:['valores de a']
+        },
+        {
+            label:'B',
+            values:['valores de B']
+        },
+        {
+            label:'C',
+            values:['valores de C']
+        }
+    ]
+    )
+
+    // config 
+    const config = {
+      type: 'venn',
+      data,
+      options: {
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 0, 0, 1)'
+        ],backgroundColor: [
+          'rgba(255, 255, 255, 0.8)',
+          'rgba(255, 255, 255, 0.8)',
+          'rgba(255, 255, 255, 0.8)',
+          'rgba(255, 255, 255, 0.8)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 255, 255, 0.8)',
+          'rgba(0, 0, 0, 0.2)'
+        ]
+      }
+    };
+
+
+
+export function buildDiagram(ncanvas){
+
+  const myChart = new Chart(
+    ncanvas,
+    config
+  );  
+  
+  return myChart;
 }
+
+  
--- main.js
+++ main.js
@@ -2,16 +2,12 @@ import 'normalize.css';
 import './style.css';
 import { buildDiagram } from './diagram';
 
-import { calculatorSets } from './calculator';
-import { validateTotalityInput, validateInputValues } from './validate/validate-input';
-import { $, $$ } from './utils/dom';
+import { validateTotalityInput } from './validate/validate-input';
+import { $ } from './utils/dom';
 
 const notABCElement = $('#elementsNotABC');
 const sampleElement = $('#sample');
-const calculatorElement = $('#calculator__diagram');
-const diagramElement = $('#canvas');
-const calculatorInputs = $$('.calculator--container__content--input');
-const calculatorSubmitElement = $('#calculator-set-submit')
+const diagramElement = $('#myChart');
 
 notABCElement.addEventListener('input', () => (
     validateTotalityInput(notABCElement, sampleElement)
@@ -20,33 +16,5 @@ notABCElement.addEventListener('input', () => (
 sampleElement.addEventListener('input', () => (
     validateTotalityInput(notABCElement, sampleElement)
 ));
-
-calculatorElement.addEventListener('submit', e => {
-    e.preventDefault();
-
-    if (!validateInputValues(calculatorInputs)) {
-        return;
-    }
-
-    if (calculatorSubmitElement.value === 'Calcular conjunto') {
-        const data = new FormData(document.calculator);
-        const results = calculatorSets(data);
-
-        calculatorInputs.forEach(input => input.disabled = true);
-
-        notABCElement.value ||= results.notABC;
-        sampleElement.value ||= results.universal;
-
-        calculatorSubmitElement.value = 'Limpiar conjunto';
-    } else {
-        calculatorInputs.forEach(input => {
-            input.value = '';
-            input.disabled = false;
-        });
-
-        calculatorSubmitElement.value = 'Calcular conjunto';
-    }
-
-});
 
 buildDiagram(diagramElement);
