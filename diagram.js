4919a3320bead0dae3bd98de4b2b9f9da90a454f
Sergio Majé <smajefranco@gmail.com>
2022-08-13 15:47:49 -0500

    :construction: Operations color in diagram

--- diagram.js
+++ diagram.js
@@ -16,7 +16,7 @@
     )
 
     // config 
-    const config = {
+    let config = {
       type: 'venn',
       data,
       options: {
@@ -40,16 +40,39 @@
       }
     };
 
+const borderColor = [
+  'rgba(255, 26, 104, 1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+  'rgba(0, 0, 0, 1)'
+]
+
+const setBackgroundColor = (backgroundColor) => {
+    config = { options: { borderColor, backgroundColor } , ...config}
+}
 
 
 export function buildDiagram(ncanvas){
+    const ctx = ncanvas.getContext('2d');
 
-  const myChart = new Chart(
-    ncanvas,
-    config
-  );  
-  
-  return myChart;
+    const myChart = new Chart(
+        ctx,
+        config
+    );
+
+    return myChart;
 }
 
+export const AUnionB = () => {
+    const backgroundColor = [];
+    setBackgroundColor(backgroundColor);
+}
+
+export const AUnionC = () => {
+    const backgroundColor = [];
+    setBackgroundColor(backgroundColor);
+}
   
