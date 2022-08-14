90f2c3ee055a6450054395bab80e6b09ee172d1d
oscarerc <oscareduardorodriguezcastro3@gmail.com>
2022-08-13 19:19:53 -0500

    agregando sombreado

--- diagram.js
+++ diagram.js
@@ -1,5 +1,6 @@
 import { $ } from './utils/dom';
 
+
 // setup
 const dataDefault = ChartVenn.extractSets([
     { label:'A', values:[] },
@@ -7,24 +8,46 @@ const dataDefault = ChartVenn.extractSets([
     { label:'C', values:[] }
 ])
 
+    // config 
+    let config = {
+      type: 'venn',
+      data,
+      options: {
+        borderColor: [
+            '#3742fa',
+            '#3742fa',
+            '#3742fa',
+            '#3742fa',
+            '#3742fa',
+            '#3742fa',
+            '#3742fa'
+        ],backgroundColor: [
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)',
+            'rgba(255, 255, 255, 0.2)'
+        ]
+      }
+    };
+
+
+
 const borderColor = [
-    'rgba(255, 26, 104, 1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-    'rgba(0, 0, 0, 1)'
+
+    '#3742fa',
+    '#3742fa',
+    '#3742fa',
+    '#3742fa',
+    '#3742fa',
+    '#3742fa',
+    '#3742fa'
+
 ]
 
-// config
-let config = {
-    type: 'venn',
-    data: dataDefault,
-    options: {
-        borderColor
-    }
-};
+
 
 const setBackgroundColor = (backgroundColor) => {
     config = { options: { borderColor, backgroundColor } , ...config}
@@ -38,11 +61,151 @@ export function buildDiagram(canvas){
 }
 
 export const AUnionB = () => {
-    const backgroundColor = [];
+    const backgroundColor = [
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(0, 0, 0, 0.2)'];
     setBackgroundColor(backgroundColor);
 }
 
 export const AUnionC = () => {
-    const backgroundColor = [];
+    const backgroundColor = [
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(0, 0, 0, 0.2)'];
     setBackgroundColor(backgroundColor);
+
+}
+
+export const BUnionC = () => {
+    const backgroundColor = [
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(0, 0, 0, 0.2)'];
+    setBackgroundColor(backgroundColor);
+}
+
+export const AInterB = () => {
+    const backgroundColor = [
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(255, 255, 255, 0.2)',
+    'rgba(0, 0, 0, 0.2)'];
+    setBackgroundColor(backgroundColor);
+}
+
+export const AInterC = () => {
+    const backgroundColor = [
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(0, 0, 0, 0.2)'];
+    setBackgroundColor(backgroundColor);
+}
+
+export const BInterC = () => {
+    const backgroundColor = [
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(0, 0, 0, 0.2)'];
+    setBackgroundColor(backgroundColor);
+}
+export const AmenosBunionC = () => {
+    const backgroundColor = [
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)']
+    setBackgroundColor(backgroundColor);
+}
+
+
+export const BmenosAunionC = () => {
+    const backgroundColor = [
+
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)']
+    setBackgroundColor(backgroundColor);
+}
+
+export const CmenosAunionB = () => {
+    const backgroundColor = [
+
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)']
+    setBackgroundColor(backgroundColor);
+}
+
+export const AunionBunionC = () => {
+    const backgroundColor = [
+
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)']
+    setBackgroundColor(backgroundColor);
+}
+export const AdiferenciasimetricBCmenosB= () => {
+    const backgroundColor = [
+
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)']
+    setBackgroundColor(backgroundColor);
+}
+  
+export const AdiferenciasimetricBCmenosC= () => {
+    const backgroundColor = [
+
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)',
+        'rgba(255, 255, 255, 0.2)']
+    setBackgroundColor(backgroundColor);
+
 }
